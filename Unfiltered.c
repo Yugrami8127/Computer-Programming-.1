dd0628971a30d46a21dae16103824257d6c041f1
Yugrami8127 <yugrami2456@gmail.com>
2025-08-29 12:28:57 +0530

    Add files via upload

---
+++ Unfiltered.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a , b ;
+    printf("First number lakh chodya: ");
+    scanf("%d",&a);
+
+    printf("Second number lakh chodya: ");
+    scanf("%d",&b);
+
+    if(a>b)
+    {
+        printf("%d is greater the %d", a,b);
+    }
+    else
+    {
+        printf("%d is greater than %d", b,a);
+    }
+    return 0;
+}
+
+
+
