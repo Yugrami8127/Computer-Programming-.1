e0c76e40b5ea739c89dc6a66b8915ac5e1bb34a7
Yugrami8127 <yugrami2456@gmail.com>
2025-09-06 11:12:39 +0530

    Add files via upload

---
+++ Celsius to  Fahreinheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float celsius , fahreinheit ;
+    printf("Enter Temperature in celsius :");
+    scanf("%f", & celsius);
+    
+    fahreinheit = celsius*9/5 + 32
+    printf("Temperature in fahreinheit = %f");
+    return 0;
+}
