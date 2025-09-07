290e93c994c2f5087b030a7648b3e1698fb0df39
Yugrami8127 <yugrami2456@gmail.com>
2025-09-07 11:23:21 +0530

    Add files via upload

---
+++ Pass or Fail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main {
+    float percentage ;
+    printf("enter the percentage you scored");
+    scanf("%f" , &percentage);
+
+    if(percentage > 35){
+        printf("You have cracked the examination");}
+
+        else{
+            printf("Sorry to say but you have failed");
+        }
+    return 0;
+}
