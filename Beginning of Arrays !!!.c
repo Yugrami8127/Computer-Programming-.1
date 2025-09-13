a3ada8abb965df9a7c2dff4d8dbed25109ba4c01
Yugrami8127 <yugrami2456@gmail.com>
2025-09-13 15:43:19 +0530

    Add files via upload

---
+++ Beginning of Arrays !!!.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){ //#1st code in arrays
+    int marks[2];
+    marks[0]=10;
+    printf("Marks of student 1 is %d\n",marks[0]);
+    marks[1]=20;
+    printf("Marks of student 2 is %d\n",marks[1]);
+    marks[2]=30;
+    printf("Marks of student 3 is %d\n",marks[2]);
+    printf("Thank you !!");
+    return 0 ;
+}
