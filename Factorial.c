f7e1eae3ec2bde85e3d5177c7abcc40dfa93a2f7
Yugrami8127 <yugrami2456@gmail.com>
2025-09-13 12:39:56 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     int n ,
+     printf("enter a number :");
+     scanf("%d, &n");
+
+     for(int i=1; i<=n; i++){
+        fact = fact * i;
+     }
+     return 0;
+}
